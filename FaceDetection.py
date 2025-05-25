20fc1dee7c39b647106e51d797d6c2225f975627
José Manuel Romero Pérez <joseromeroperez1909@gmail.com>
2025-05-25 17:00:38 -0400

    Small changes (for #5)
    Clearing/Adding some spaces white spaces, and making the code look cleaner in general. 
    
    Also, added a small comment on the break loop statement, to understand which keys are '82' (q) and '113' (F2)

--- FaceDetection.py
+++ FaceDetection.py
@@ -7,29 +7,20 @@ cascade = cv2.CascadeClassifier(alg) # loading the model with cv2
 cam = cv2.VideoCapture(0) # initialization camera
 
 while True:
+    _, img = cam.read() # read the frame from the camera
     
-    _,img = cam.read() # read the frame from the camera
-    
-    grayImg = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY) # converting color into gray scale image
+    grayImg = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) # converting color into gray scale image
 
     face = cascade.detectMultiScale(grayImg) # get coordinates of face
 
     for (x, y, w, h) in face: # segregating x,y,w,h
-        
-        cv2.rectangle(img, (x, y), (x + w, y + h),(0,255,0),2) # draw the retangle
+        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2) # draw the retangle
 
-    cv2.imshow("FaceDetect",img)
+    cv2.imshow("FaceDetect", img)
 
-    key = cv2.waitKey(1)
-
-    if key == 81 or key == 113 :
+    key = cv2.waitKey(1) 
+    if key == 81 or key == 113: # 81: 'q', 113: 'F2'
         break
 
 cam.release()
 cv2.destroyAllWindows()
-        
-
-    
-
- 
-
